bf7d9693ed67630cb2e058e39add0566d99b2101
Marija-Milenkoska <marija.milenkoska15@gmail.com>
2024-06-28 10:42:01 +0200

    Update calculator.py

--- Auditory/Auditory 1/calculator.py
+++ Auditory/Auditory 1/calculator.py
@@ -1,1 +1,24 @@
+#Напишете функција која ќе ги содржи функционалностите на едноставен аритметички калкулатор.
 
+operators = ["+", "-", "*", "/", "%"]
+
+def calculator(a, b, operator):
+    if operator not in operators:
+        return "Invalid operator"
+
+    if operator == "+":
+        return a + b
+    if operator == "-":
+        return a - b
+    if operator == "*":
+        return a * b
+    if operator == "/":
+        return a / b
+    if operator == "%":
+        return a % b
+
+if __name__ == "__main__":
+    a = float(input("Enter first number:"))
+    operator = input("Enter an operator: ")
+    b = float(input("Enter second:"))
+    print(calculator(a, b, operator))
